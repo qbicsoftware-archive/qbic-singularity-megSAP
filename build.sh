6ee0d987de00d9f71ef4c8b956eeb0c8b6a02b66
Alexander Peltzer <alexander.peltzer@uni-tuebingen.de>
2017-10-25 16:11:42 +0200

    NOw with working build script

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 #install dependencies
+apt-get update -y 
 apt-get install -y wget bzip2 unzip make g++ git cmake tabix build-essential qt5-default qt5-qmake qtbase5-dev libqt5sql5-mysql libqt5xmlpatterns5-dev php7.0-cli php7.0-xml php7.0-mysql python python-matplotlib libncurses5-dev bzip2 libbz2-dev liblzma-dev
