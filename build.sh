8be679fd9263f5f4704e49bc370bf11c86756e27
Alexander Peltzer <alexander.peltzer@uni-tuebingen.de>
2017-10-25 16:30:57 +0200

    Updated container paths for qbic

--- build.sh
+++ build.sh
@@ -3,3 +3,7 @@
 #install dependencies
 apt-get update -y 
 apt-get install -y wget bzip2 unzip make g++ git cmake tabix build-essential qt5-default qt5-qmake qtbase5-dev libqt5sql5-mysql libqt5xmlpatterns5-dev php7.0-cli php7.0-xml php7.0-mysql python python-matplotlib libncurses5-dev bzip2 libbz2-dev liblzma-dev
+
+mkdir -p /lustre_cfc/qbic/megSAP-data
+mkdir -p /lustre_cfc/ws_qstor/ws
+mkdir -p /home-link
